7c88af8b53b0c1c35578ec47bb81a01f6840d0e4
ssant096 <ssant096@ucr.edu>
2022-11-12 20:34:09 -0800

    Updated header

--- header/Filter.h
+++ header/Filter.h
@@ -2,13 +2,18 @@
 #define __FILTER_H__
 
 #include <string>
+//#include "../src/Movies.cpp"
 
 class Filter{
+private:
+    Movies** listToModify;
 public:
-    void sortByViews();
-    void sortByGenre(string movieGenre);
-    void sortByYear();//reads through MoviesList and sorts Movies based on year (most recent to oldest)
-    void findSimilarMovies();
+    Movies** sortByViews();//reads through list of movies and sorts by views (most to fewest) and returns new sorted list
+    Moives** sortByGenre(string movieGenre);//reads through list and sorts Movies based on year (most recent to oldest)
+    Movies** sortByYear();//sorts list based on year (most recent to oldest) and returns new list
+    void findSimilarMovies(string movieTitle);//reads through list and finds similar movies and returns new list
+    Filter(Movies** listOfMovies);//constructor, copies entered list of movies to a list which will be modified
+    Filter();//default constructor which will initialize an empty list
 };
 
 #endif

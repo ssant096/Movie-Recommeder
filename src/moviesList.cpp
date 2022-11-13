3986e23b6c3896f5cbb3011ce176939c6153b636
hirschheda <hirschchheda@gmail.com>
2022-11-13 00:07:48 -0800

    created moviesList class

---
+++ src/moviesList.cpp
@@ -0,0 +1,21 @@
+#include "moviesList.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+MoviesList::MoviesList(string filterType)
+{
+
+}
+
+MoviesList::MoviesList(Database* fullDatabase)
+{
+
+}
+
+void MoviesList::printListOfMovies()
+{
+    
+}

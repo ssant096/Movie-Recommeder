3f8c3d49d89728ead48fa9549affeb6beb0abb2f
tcheu024 <tim31307@gmail.com>
2022-11-16 21:06:34 -0800

    Fixed major bugs relatimg to constructor

--- header/movies.h
+++ header/movies.h
@@ -1,22 +1,26 @@
+#include <string>
+
+using namespace std;
+
 #ifndef __MOVIES_H__
 #define __MOVIES_H__
 
-class Movies{
-    private:
-
-        string title; //title of the movie
-        string genre; //genre of the movie
-        double rating; // rating of the movie out of 10
-        int releaseDate; //the year the movie was released
-        int views; // amount of viewership
+class Movies {
+private:
+  string title;    // title of the movie
+  string genre;    // genre of the movie
+  double rating;   // rating of the movie out of 10
+  int releaseDate; // the year the movie was released
+  int length;      // amount of viewership
 
 public:
-        Movies(string title, int rating, string genre, int releaseDate, int length); 
-        double getRating();
-        string getTitle();
-        string Genre();
-        int getReleaseDate();
-        int getLength();
+  Movies(string title, string genre, int releaseDate, int length,
+         double rating);
+  double getRating();
+  string getTitle();
+  string Genre();
+  int getReleaseDate();
+  int getLength();
+};
 
-}
-
+#endif
--- src/movies.cpp
+++ src/movies.cpp
@@ -4,36 +4,21 @@
 
 using namespace std;
 
-Movies::Movies(string title_, string genre_, int releaseDate_, int length _, double rating_){
-    title = title_;
-    genre = genre_;
-    releaseDate = releaseDate_;
-    length= length_;
-    rating= rating_;
+Movies::Movies(string title_, string genre_, int releaseDate_, int length_,
+               double rating_) {
+  title = title_;
+  genre = genre_;
+  releaseDate = releaseDate_;
+  length = length_;
+  rating = rating_;
 }
 
-double Movies::getRating(){
-    return this->rating;
+double Movies::getRating() { return this->rating; }
 
-}
+string Movies::getTitle() { return this->title; }
 
-string Movies::getTitle(){
-    return this->title;
+string Movies::Genre() { return this->genre; }
 
+int Movies::getReleaseDate() { return this->releaseDate; }
 
-}
-
-string Movies::Genre(){
-    return this->genre;
-
-}
-
-int Movies::getReleaseDate(){
-    return this->releaseDate;
-
-}
-
-int Movies::getLength(){
-    return this->length;
-
-}
+int Movies::getLength() { return this->length; }

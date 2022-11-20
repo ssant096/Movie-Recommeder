cd80a25ba7f47ce1f1f90caa8321e1c73ef4ee6c
hirschheda <hirschchheda@gmail.com>
2022-11-19 23:53:11 -0800

    update filter function

--- src/moviesList.cpp
+++ src/moviesList.cpp
@@ -2,12 +2,22 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include "../header/filter.h"
 using namespace std;
 
 MoviesList::MoviesList(string filterType)
 {
-
+    if(filterType=="genre"||filterType=="Genre")
+    {
+        cout<<"Enter which genre\n";
+        string genre;
+        cin>>genre;
+        sortByGenre(genre);
+    }
+    else if(filterType=="year"||filterType=="Year")
+        sortByYear();
+    else if(filterType=="length"||filterType=="Length")
+        sortByLength();
 }
 
 MoviesList::MoviesList(Database* fullDatabase)

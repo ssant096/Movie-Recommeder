222c78e4539cc9f740d7224c1f09f364e8975de2
ssant096 <102336530+ssant096@users.noreply.github.com>
2022-11-21 07:12:40 -0800

    Update moviesList.cpp

--- src/moviesList.cpp
+++ src/moviesList.cpp
@@ -1,34 +1,51 @@
-#include "../header/moviesList.h"
 #include <iostream>
-#include <string>
+#include <cstring>
 #include <vector>
-//#include "../header/filter.h"
+#include "../header/moviesList.h"
+#include "../header/database.h"
+#include "../header/movies.h"
+
 using namespace std;
 
-MoviesList::MoviesList(string filterType)
-{
-    if(filterType=="genre"||filterType=="Genre")
-    {
-        cout<<"Enter which genre\n";
-        string genre;
-        cin>>genre;
-        //sortByGenre(genre);
+MoviesList::MoviesList(){
+    movieDatabase.loadMovies();
+    listOfMovies = movieDatabase.returnDatabaseList();
+}
+
+MoviesList::MoviesList(vector<Movies> listToFilter, string filterType){
+    listOfMovies = listToFilter;
+
+    if(filterType == "rating"){
+        filterList = Filter(listOfMovies);
+        listOfMovies = filterList.sortByRating();
     }
-    else if(filterType=="year"||filterType=="Year")
-        //sortByYear();
-    else if(filterType=="length"||filterType=="Length")
-        //sortByLength();
+    else if(filterType == "length"){
+        filterList = Filter(listOfMovies);
+        listOfMovies = filterList.sortByLength();
+    }
+    else if(filterType == "year"){
+        filterList = Filter(listOfMovies);
+        listOfMovies = filterList.sortByYear();
+    }
 }
 
-MoviesList::MoviesList(Database* fullDatabase)
-{
-
+void MoviesList::filterByGenre(string genre){
+    filterList = Filter(listOfMovies);
+    listOfMovies = filterList.sortByGenre(genre);
 }
 
-void MoviesList::printListOfMovies()
-{
+void MoviesList::filterByName(string movieTitle){
+    filterList = Filter(listOfMovies);
+    listOfMovies = filterList.findSimilarMovies(movieTitle);
+}
+
+void MoviesList::printListOfMovies(){
     for(unsigned i=0; i<listOfMovies.size();i++)
     {
-        cout<<listOfMovies[i].getTitle()<<", "<<listOfMovies[i].getReleaseDate()<<", "<<listOfMovies[i].getGenre()<<", "<<listOfMovies[i].getGenre()<<", "<<listOfMovies[i].getLength();
+        cout<<listOfMovies[i].getTitle()<<", "<<listOfMovies[i].Genre()<<", "<<listOfMovies[i].getReleaseDate()<< ", "<<listOfMovies[i].getLength() <<", "<< listOfMovies[i].getRating() << endl;
     }
+}
+
+vector<Movies> MoviesList::returnMoviesList(){
+    return listOfMovies;
 }

29cdbb84fc1618286c28a135db2ed09fff8c9c45
tcheu024 <tim31307@gmail.com>
2022-11-26 18:04:10 -0800

    Unit Test for Movies Class

---
+++ tests/movies_test.cpp
@@ -0,0 +1,34 @@
+#include "gtest/gtest.h"
+#include "../header/filter.h"
+#include "../header/movies.h"
+#include <vector>
+#include <string>
+
+TEST(moviesTest, testConstructor) {
+    EXPECT_DEATH (Movies *aMovie = new Movies("Title", "Genre", 2020, 120, 8.0);
+}
+
+TEST(moviesTest, testGetRating ) {
+    Movies *aMovie = new Movies("Title", "Genre", 2020, 120, 8.0);
+    EXPECT_EQ (aMovie->getRating(),8.0);
+}
+
+TEST(moviesTest, testGenre) {
+    Movies *aMovie = new Movies("Title", "Genre", 2020, 120, 8.0);
+    EXPECT_EQ (aMovie->Genre(), "Genre");
+}
+
+TEST(moviesTest, testGetTitle) {    
+    Movies *aMovie = new Movies("Title", "Genre", 2020, 120, 8.0);
+    EXPECT_EQ (aMovie->getRating(), "Title");
+}
+
+TEST(moviesTest, testGetReleaseDate) {
+    Movies *aMovie = new Movies("Title", "Genre", 2020, 120, 8.0);
+    EXPECT_EQ (aMovie->getReleaseDate(), 2020);
+}
+
+TEST(moviesTest, testGetLength) {
+    Movies *aMovie = new Movies("Title", "Genre", 2020, 120, 8.0);
+    EXPECT_EQ (aMovie->getLength(), 120);
+}

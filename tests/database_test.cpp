40c58d4c90bb4b495229896c5612f87f765853fe
PixlYoshi <ysing015@ucr.edu>
2022-11-27 16:13:54 -0800

    add all tests for database

--- tests/database_test.cpp
+++ tests/database_test.cpp
@@ -4,9 +4,24 @@
 #include <string>
 
 TEST(databaseTest, testConstructor){
-    EXPECT_NO_THROW (Database Database);
+    EXPECT_NO_THROW (Database initDatabase());
 }
 
 TEST(databaseTest, testConstructor2){
-    EXPECT_NO_THROW (Database initDatabase);
+    EXPECT_NO_THROW (Database Database());
+}
+
+TEST(databaseTest, testAddFunction){
+    Database Database();
+    EXPECT_NO_THROW (Database addMovieToList(test, Action, 2011, 120, 8.9));
+}
+
+TEST(databaseTest, testDeleteFunction){
+    Database Database();
+    EXPECT_NO_THROW (Database deleteLastMovieFromList());
+}
+
+TEST(databaseTest, testReturnFunction){
+    Database Database();
+    EXPECT_NO_THROW (Database returnDatabaseList());
 }
